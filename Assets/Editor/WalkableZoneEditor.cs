9ab30d9cec222889993462ace647d4cbebba384b
Valerazz2 <107555043+Valerazz2@users.noreply.github.com>
2023-07-06 21:09:33 +0300

    WalkableZoneEditor.cs is ready

--- Assets/Editor/WalkableZoneEditor.cs
+++ Assets/Editor/WalkableZoneEditor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Common.Unity.Util.Math;
 using Common.Util.Math;
+using Iso.Cells;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEditor.EditorTools;
@@ -13,12 +14,14 @@ public class WalkableZoneEditor : EditorTool
    
    public Texture2D icon;
    [SerializeField] GameObject buildAble, walkAble, closed;
-   [SerializeField] float gridXLength;
-   [SerializeField] float gridYLength;
+
+   private int mapHeigth = 100, mapWidth = 100;
    private GameObject sprite;
    private int colorIndex;
    private GameObject grid;
    private IsometricProjector isometricProjector = new();
+   private Cells cells;
+   private CellType currentType = CellType.Buildable;
 
    /*public override GUIContent toolbarIcon =>
       new()
@@ -31,9 +34,11 @@ public class WalkableZoneEditor : EditorTool
     {
        get
        {
-          grid = FindObjectOfType<Grid>().gameObject;
-          gridXLength = grid.GetComponent<Grid>().cellSize.x / 2;
-          gridYLength = grid.GetComponent<Grid>().cellSize.y / 2;
+          if (cells == null)
+          {
+             cells = new Cells();
+             cells.Create(mapWidth, mapHeigth);
+          }
           return new GUIContent
           {
              image = icon,
@@ -58,21 +63,13 @@ public class WalkableZoneEditor : EditorTool
       
       Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
       Vector3 worldMousePos = ray.GetPoint(10f);
-      Debug.Log("WorldMousePos = " + worldMousePos);
-      
-      
       
       var xM = isometricProjector.v2mx(worldMousePos.x, worldMousePos.y);
       var yM = isometricProjector.v2my(worldMousePos.x, worldMousePos.y);
-      var posFloat = new Vector3(xM, yM, 0);
-      Debug.Log("ModelPos = " + posFloat);
-      var pos = new Vector3((int) posFloat.x, (int) posFloat.y, 0);
-      Debug.Log("PosToInt = " + pos);
-      var xV = isometricProjector.m2vx(pos.x, pos.y);
-      var yV = isometricProjector.m2vy(pos.x, pos.y);
-      worldMousePos = new Vector3(xV, yV, 0);
-      Debug.Log("FinallyViewPos = " + worldMousePos);
-      sprite.transform.position = worldMousePos;
+      var xV = isometricProjector.m2vx(Mathf.FloorToInt(xM), Mathf.FloorToInt(yM));
+      var yV = isometricProjector.m2vy(Mathf.FloorToInt(xM), Mathf.Floor(yM));
+      var roundedPos = new Vector3(xV, yV, 0);
+      sprite.transform.position = roundedPos;
       
       
       
@@ -84,21 +81,40 @@ public class WalkableZoneEditor : EditorTool
          DestroyImmediate(sprite);
          switch (colorIndex)
          {
-            case 0: sprite = Instantiate(buildAble, grid.transform); 
+            case 0: sprite = Instantiate(buildAble, grid.transform);
+               currentType = CellType.Buildable;
                break;
             case 1: sprite = Instantiate(walkAble, grid.transform);
+               currentType = CellType.Traversable;
                break;
             case 2: sprite =  Instantiate(closed, grid.transform);
+               currentType = CellType.Blocked;
                break;
          }
 
-         sprite.transform.position = worldMousePos;
+         sprite.transform.position = roundedPos;
       }
 
+      if (Event.current.type == EventType.KeyDown)
+      {
+         Debug.Log("KeyUp");
+      }
+      if (Event.current.type == EventType.KeyUp)
+      {
+         Debug.Log("KeyUp");
+      }
+      if (Event.current.type == EventType.MouseDown)
+      {
+         Debug.Log("KeyUp");
+      }
       if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
       {
-         var newSprite = Instantiate(sprite, worldMousePos, quaternion.identity, grid.transform);
-         newSprite.name = "C(posX, posY)";
+         if (cells.Get((int) xM, (int) yM) == null)
+         {
+            cells.Set((int) xM, (int) yM, currentType);
+            var newSprite = Instantiate(sprite, roundedPos, quaternion.identity, grid.transform);
+            newSprite.name = $"Cell(X:{roundedPos.x}, Y:{roundedPos.y})";
+         }
       }
    }
 }
