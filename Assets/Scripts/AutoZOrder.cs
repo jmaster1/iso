866c6b5212adf496de500cdc3c1bf73e6aad5778
timur <timur.kozlov@gmail.com>
2023-07-09 17:54:05 +0300

    AutoZOrder upd

--- Assets/Scripts/AutoZOrder.cs
+++ Assets/Scripts/AutoZOrder.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -10,22 +7,17 @@ public class AutoZOrder : MonoBehaviour
     private void Update()
     {
         zOrder = 0;
-        var childCount = transform.childCount;
-        for (int i = 0; i < childCount; i++)
-        {
-            var child = transform.GetChild(i);
-            SetZOrder(child);
-        }
+        SetZOrder(transform);
     }
 
     private void SetZOrder(Transform obj)
     {
-        if (obj.TryGetComponent(out SpriteRenderer sr))
+        if (obj.TryGetComponent(out Renderer sr))
         {
             sr.sortingOrder = zOrder--;
         }
 
-        for (int i = 0; i < obj.childCount; i++)
+        for (var i = 0; i < obj.childCount; i++)
         {
             SetZOrder(obj.GetChild(i));
         }
