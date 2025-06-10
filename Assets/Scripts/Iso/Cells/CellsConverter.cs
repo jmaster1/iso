d3ce4180c71d70922f330d2c39f4da2d76d4f071
timur.kozlov <timur.kozlov@hireright.com>
2025-06-10 15:10:04 +0300

    CellsConverter

--- Assets/Scripts/Iso/Cells/CellsConverter.cs
+++ Assets/Scripts/Iso/Cells/CellsConverter.cs
@@ -12,12 +12,11 @@ namespace Iso.Cells
     public class CellsConverter : JsonConverterGeneric<PooledObsList<Cell>>
     {
         private IsoWorld _world;
+        
         public CellsConverter(IsoWorld world)
         {
             _world = world;
         }
-
-        //Map<ResourceType, long> map = new Map<ResourceType, long>();
 
         protected override void WriteJson(JsonWriter writer, PooledObsList<Cell> value, JsonSerializer serializer)
         {
@@ -35,43 +34,31 @@ namespace Iso.Cells
                 data.Write(cell.y);
                 data.Write((int)cell.CellType);
             }
-
-            writer.WriteStartObject();
-            writer.WritePropertyName("cells");
+            data.Flush();
             writer.WriteValue(ms.ToBase64String());
-            writer.WriteEndObject();
         }
 
         protected override PooledObsList<Cell>? ReadJson(JsonReader reader, PooledObsList<Cell>? value, JsonSerializer serializer)
         {
-            if (reader.IsNull()) return value;
-            LangHelper.Validate(reader.IsStartObject());
+            var cells = _world.Cells;
+            cells.Clear();
             
-            reader.Read();
-            while (reader.IsPropertyName())
+            if (reader.IsNull()) return value;
+            LangHelper.Validate(reader.IsString());
+            var b64 = (string)reader.Value!;
+            
+            var data = b64.FromBase64StringStream().GUnZip().DataReader();
+            var w = data.ReadInt32();
+            var h = data.ReadInt32();
+            cells.Create(w, h);
+            var cellCount = data.ReadInt32();
+            for (var i = 0; i < cellCount; i++)
             {
-                var name = (string)reader.Value!;
-                if ("cells".Equals(name))
-                {
-                    var cells = _world.Cells;
-                    var list = cells.CellList;
-                    var b64 = reader.ReadAsString()!;
-                    var data = b64.FromBase64StringStream().GUnZip().DataReader();
-                    var w = cells.Width = data.ReadInt32();
-                    var h = cells.Heigth = data.ReadInt32();
-                    var cellCount = data.ReadInt32();
-                    for (var i = 0; i < cellCount; i++)
-                    {
-                        var x = data.ReadInt32();
-                        var y = data.ReadInt32();
-                        var cellType = data.ReadInt32();
-                        cells.Set(x, y, (CellType)cellType);
-                    }
-
-                } else reader.Skip();
-                reader.Read();
+                var x = data.ReadInt32();
+                var y = data.ReadInt32();
+                var cellType = data.ReadInt32();
+                cells.Set(x, y, (CellType)cellType);
             }
-            LangHelper.Validate(reader.IsEndObject());
             return value;
         }
     }
