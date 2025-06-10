3e6b8ccfbe08c14640461ff5ad1f258eb53dff1f
timur <timur.kozlov@gmail.com>
2025-06-10 19:48:17 +0300

    BuildingsConverter rip

--- Assets/Scripts/Iso/Buildings/BuildingsConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Common.IO.Serialize.Newtonsoft.Json.Converter;
-using Common.Lang.Observable;
-
-namespace Iso.Buildings
-{
-    public class BuildingsConverter : NonAddingArrayConverter<PooledObsList<Building>, Building>
-    {
-    }
-}
